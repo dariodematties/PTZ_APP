3acacdfd9bb757c6d079e69a9e5faf29515941d7
dariodematties <ddematties@anl.gov>
2024-11-06 21:58:58 +0000

    Some mnor fixes

--- main.py
+++ main.py
@@ -25,6 +25,13 @@ def get_argparser():
         default=5,
     )
     parser.add_argument(
+        "-obj",
+        "--object",
+        help="The object to capture with the camera.",
+        type=str,
+        default="animal",
+    )
+    parser.add_argument(
         "-un",
         "--username",
         help="The username of the PTZ camera.",
@@ -63,13 +70,13 @@ def get_argparser():
 def look_for_object(args):
     #device = "cuda:0" if torch.cuda.is_available() else "cpu"
     #torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
-
     device = "cpu"
     torch_dtype = torch.float32
 
     #object_ = 'robot object'
     #object_ = 'very tiny and distant objects'
-    object_ = 'a camera dome'
+    #object_ = 'a camera dome'
+    object_ = args.object
     #pans = [150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150]
     pans = [0, 15, 30, 45, 60, 75, 90, 105, 120, 135, 150, 165, 180, 195, 210, 225, 240, 255, 270, 285, 300, 315, 330, 345]
     tilts = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
@@ -84,33 +91,36 @@ def look_for_object(args):
     tilts = [0, 0, 0, 0, 0]
     zooms = [1, 1, 1, 1, 1]
 
-    for pan, tilt, zoom in zip(pans, tilts, zooms):
-        policy_net=None
-        image_path, LABEL = get_image_from_ptz_position(args, object_, pan, tilt, zoom)
-        print(image_path)
-        if LABEL is None:
-            print('<<<<<<>>>>>>---------------------<<<<<<>>>>>>')
-            print('<<<<<<>>>>>>---------------------<<<<<<>>>>>>')
-            print(f'             no object found                ')
-            print('<<<<<<>>>>>>---------------------<<<<<<>>>>>>')
-            print('<<<<<<>>>>>>---------------------<<<<<<>>>>>>')
-        else:
-            # Chooses the label to follow
-            label = LABEL['label']
-            bbox = LABEL['bbox']
+    
+    for iteration in range(args.iterations):
+        for pan, tilt, zoom in zip(pans, tilts, zooms):
+            policy_net=None
+            image_path, LABEL = get_image_from_ptz_position(args, object_, pan, tilt, zoom)
             reward = LABEL['reward']
-            print('reward: ', reward)
-            print('type(reward): ', type(reward))
-            image = Image.open(image_path)
-            print('<<<<<<>>>>>>---------------------<<<<<<>>>>>>')
-            print('<<<<<<>>>>>>---------------------<<<<<<>>>>>>')
-            print(f'         following {label} object           ')
-            print('<<<<<<>>>>>>---------------------<<<<<<>>>>>>')
-            print('<<<<<<>>>>>>---------------------<<<<<<>>>>>>')
-            os.remove(image_path)
-            center_and_maximize_object(args, bbox, image, reward)
+            print(image_path)
+            if LABEL is None or reward > 0.99:
+                print('<<<<<<>>>>>>---------------------<<<<<<>>>>>>')
+                print('<<<<<<>>>>>>---------------------<<<<<<>>>>>>')
+                print(f'             no object found                ')
+                print('<<<<<<>>>>>>---------------------<<<<<<>>>>>>')
+                print('<<<<<<>>>>>>---------------------<<<<<<>>>>>>')
+            else:
+                # Chooses the label to follow
+                label = LABEL['label']
+                bbox = LABEL['bbox']
+                reward = LABEL['reward']
+                print('reward: ', reward)
+                print('type(reward): ', type(reward))
+                image = Image.open(image_path)
+                print('<<<<<<>>>>>>---------------------<<<<<<>>>>>>')
+                print('<<<<<<>>>>>>---------------------<<<<<<>>>>>>')
+                print(f'         following {label} object           ')
+                print('<<<<<<>>>>>>---------------------<<<<<<>>>>>>')
+                print('<<<<<<>>>>>>---------------------<<<<<<>>>>>>')
+                os.remove(image_path)
+                center_and_maximize_object(args, bbox, image, reward)
 
-    publish_images()
+        publish_images()
 
 
 
